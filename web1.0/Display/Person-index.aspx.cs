adf75c131e11f3d95c3a97151aa3b03f7e6140d9
LYF66666 <924483784@qq.com>
2017-10-17 12:24:10 +0800

    123

--- web1.0/Display/Person-index.aspx.cs
+++ web1.0/Display/Person-index.aspx.cs
@@ -10,7 +10,6 @@ public partial class Display_Person_index : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        Session["TeacherNumber"] = 1;
         try
         {
             //判断是不是评审
